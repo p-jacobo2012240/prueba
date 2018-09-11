50b760e66131bf363e63010b495abd8ab6b61e10
Pablo Jacobo <pablojacobo1998@gmail.com>
2018-09-11 13:20:01 -0600

    Users  & Starships

--- funcionalidades.js
+++ funcionalidades.js
@@ -1,14 +1,26 @@
 const API_URL_STW = 'https://swapi.co/api/';
 const API_URL_CWS = 'https://www.codewars.com/api/v1/';
 const PERSONA_URL = 'people/:id'
+const NAVES = 'starships/:numero';
 
+let yo = 'Pablo Daniel';
+
+//Personajes
 const USER_ONE =  `${API_URL_STW}${PERSONA_URL.replace(':id', 1 )} `;
+//Naves
+const NAVE_ESPACIAL = `${API_URL_STW}${NAVES.replace(':numero', 2)}`;
+
+// SIMILAR CORS 
 const OPT = { crossDomain: true };
 
 $.get( USER_ONE, OPT, (personaje)=>{
     console.log(`Hola soy: ${personaje.name} `)
 }); 
 //console.log(`Hola soy: ${USER_ONE}`)
+
+$.get( NAVE_ESPACIAL, OPT, (nave)=>{
+    console.log(`${yo} Manejo la ${nave.name} por el Universo`)
+});
 
 //El comportamiento asincrono JS <3
 console.log('LORD JCOB');
